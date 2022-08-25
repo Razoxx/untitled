5e448a27aad1662f77f8dff19f97b7aa369711f9
Rosterik <rifanwot@gmail.com>
2022-08-25 15:13:30 +0300

    nam

---
+++ src/main/java/com/lesson/calc/Calculator.java
@@ -0,0 +1,10 @@
+package com.lesson.calc;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class Calculator {
+
+    JButton myButton = new JButton();
+    JFrame myFrame = new JFrame();
+}
---
+++ src/main/java/com/lesson/converter/TempConverter.java
@@ -0,0 +1,7 @@
+package com.lesson.converter;
+
+public class TempConverter {
+
+
+
+}
