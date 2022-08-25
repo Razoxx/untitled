f3f84f82431944307d1eca4a707f9b56d435c0ef
Rosterik <rifanwot@gmail.com>
2022-08-25 15:40:09 +0300

    nam

--- src/main/java/com/lesson/nam.java
+++ src/main/java/com/lesson/nam.java
@@ -3,34 +3,26 @@ package com.lesson;
 import java.util.Scanner;
 class MyProgram {
     public static void main(String[] args) {
-        double a, b , c;
-        double D;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Введите значение b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Введите значение c: ");
+        double c = scanner.nextDouble();
 
+        double d = b * b - 4 * a * c;
 
-        System.out.println("Программа решает квадратное уравнение вида:");
-        System.out.println("ax^2 + bx + c = 0");
-        System.out.println("Введите a, b и c:");
-
-        Scanner in = new Scanner(System.in);
-
-        a = in.nextDouble();
-        b = in.nextDouble();
-        c = in.nextDouble();
-
-        D = b * b - 4 * a * c;
-        if (D > 0) {
-            double x1, x2;
-            x1 = (-b - Math.sqrt(D)) / (2 * a);
-            x2 = (-b + Math.sqrt(D)) / (2 * a);
+        if (d > 0) {
+            double x1 = (-b - Math.sqrt(d)) / (2 * a);
+            double x2 = (-b + Math.sqrt(d)) / (2 * a);
             System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
-        }
-        else if (D == 0) {
+        } else if (d == 0) {
             double x;
             x = -b / (2 * a);
             System.out.println("Уравнение имеет единственный корень: x = " + x);
-        }
-        else {
-            System.out.println("Уравнение не имеет действительных корней!");
+        } else {
+            System.out.println("Уравнение не имеет действительных корней");
         }
     }
 }
